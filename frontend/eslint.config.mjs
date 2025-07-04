04c50ef13b7768c0aba645316f08f916dbf61095
Darya Korbut <d.korbut@godeltech.com>
2025-07-04 14:36:57 +0300

    feat: update package.json for linting and formatting improvements

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,41 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
+
+export default tseslint.config([
+    globalIgnores(['dist']),
+    {
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            parser: tseslint.parser,
+            globals: globals.browser,
+            parserOptions: {
+                sourceType: 'module',
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+        plugins: {
+            react,
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+        },
+        rules: {
+            ...react.configs.recommended.rules,
+            ...reactHooks.configs['recommended-latest'].rules,
+            ...reactRefresh.configs.vite.rules,
+            'react/react-in-jsx-scope': 'off',
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+    },
+])
